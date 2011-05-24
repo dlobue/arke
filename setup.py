6a6686af5a063602255c35df8acb907ef5f12de3
Dominic LoBue <dominic.lobue@gmail.com>
2011-05-24 13:11:22 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='cagentd',
+      version=version,
+      description="basic server agent for collecting stats",
+      long_description="""\
+TODO""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Dominic LoBue',
+      author_email='dominic@geodelic.com',
+      url='',
+      license='proprietary',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

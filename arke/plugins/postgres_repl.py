4ffb63f6789c80396860469807aab2eacb7fe0bc
Dominic LoBue <dominic@geodelic.com>
2011-07-27 15:28:58 -0700

    Make plugin more robust and able to survive postgres server restarts.

--- arke/plugins/postgres_repl.py
+++ arke/plugins/postgres_repl.py
@@ -1,7 +1,14 @@
 
+import logging
+
 import psycopg2
+from eventlet import sleep
 
 from arke.plugin import collect_plugin
+
+MAX_ATTEMPTS = 5
+
+class NoConnection(Exception): pass
 
 class postgres_repl(collect_plugin):
     name = "postgres_repl"
@@ -41,6 +48,13 @@ class postgres_repl(collect_plugin):
                 hoststr = host
                 port = None
 
+            if host in conns:
+                try:
+                    conns[host].set_session(readonly=True, autocommit=True)
+                except psycopg2.OperationalError:
+                    #lost the connection, so remove it from pool
+                    conns.pop(host)
+
             if host not in conns:
                 connect_params = dict(
                     host=hoststr,
@@ -53,26 +67,41 @@ class postgres_repl(collect_plugin):
                 conns[host] = psycopg2.connect(
                     **connect_params
                 )
+                conns[host].set_session(readonly=True, autocommit=True)
             yield host, conns[host]
 
-    def run(self):
+    def run(self, attempt=0):
         result = {}
         for host,connection in self.iter_connections():
             cursor = connection.cursor()
             try:
-                #raises OperationalError on slave
                 cursor.execute('SELECT pg_current_xlog_location()')
                 result['master'] = cursor.fetchone()[0]
             except psycopg2.OperationalError:
-                connection.rollback()
-                #returns none,none on solo and master
-                cursor.execute('SELECT pg_last_xlog_receive_location(), pg_last_xlog_replay_location()')
+                if connection.closed:
+                    result = None
+                    break
+                try:
+                    cursor.execute('SELECT pg_last_xlog_receive_location(), pg_last_xlog_replay_location()')
+                except psycopg2.OperationalError:
+                    if connection.closed:
+                        result = None
+                        break
                 slaves = result.setdefault('slaves', [])
                 d = {'host': host}
                 d['r'], d['p'] = cursor.fetchone()
                 slaves.append(d)
             finally:
                 cursor.close()
+
+        if result is None:
+            if attempt > MAX_ATTEMPTS:
+                logging.error("Connection to postgres servers was interrupted more than %i times - going to bail.")
+                raise NoConnection
+            else:
+                logging.error("Lost connection to postgres server %s while retrieving WAL location. Going to try again." % host)
+                sleep(5)
+                return self.run(attempt+1)
 
         return result
 

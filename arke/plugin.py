427b4279be5bbbe70003afa2774f28dae7bf94ce
Dominic LoBue <dominic@geodelic.com>
2011-08-04 06:01:27 -0700

    finished plugin_manager component, and base collect_plugin is nearly complete.

--- arke/plugin.py
+++ arke/plugin.py
@@ -6,7 +6,10 @@ from glob import glob
 import imp
 import os
 import sys
+from functools import partial
+from inspect import getmembers, getmodule, isclass
 
+from pprint import pformat
 
 try:
     from pkg_resources import working_set
@@ -16,60 +19,50 @@ except ImportError:
     ENTRY_POINTS = False
 
 from bson import json_util, BSON
-#from giblets import Component, ComponentManager, ExtensionPoint, implements
-#from giblets.policy import Blacklist
-#from giblets.search import find_plugins_by_entry_point, find_plugins_in_path
 from circuits import Component, Event, Timer
 
 
-class GatherData(Event): pass
-
-
-def get_plugin_manager(search_paths=None):
-    mgr = PluginManager()
-    blacklist = Blacklist()
-    blacklist.disable_component(collect_plugin)
-    blacklist.disable_component(multi_collect_plugin)
-    mgr.restrict(blacklist)
-    find_plugins_by_entry_point('arke_plugins')
-    if search_paths:
-        find_plugins_in_path(search_paths)
-    return mgr
-
-#class PluginManager(ComponentManager):
-    #collection_plugins = ExtensionPoint(icollecter)
-    #compmgr = property(lambda self: self)
-
 
 class collect_plugin(Component):
-    #implements(icollecter)
     default_config = {'interval': 30}
+    config = None
 
-    @property
-    def config(self):
-        try:
-            return config.get_config()
-        except config.NoSettings:
-            return None
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super(collect_plugin, cls).__new__(
+                                cls, *args, **kwargs)
+        return cls._instance
 
     def get_setting(self, setting, fallback=None, opt_type=None):
-        if self.config and self.config.has_option(self.section, setting):
+        val = None
+        logging.debug("getting settings")
+        if self.manager is not self or 'config' in self.components:
             try:
-                getter = {int: self.config.getint,
-                          float: self.config.getfloat,
-                          bool: self.config.getboolean,
+                #getter = {int: self.config.getint,
+                          #float: self.config.getfloat,
+                          #bool: self.config.getboolean,
+                         #}[opt_type]
+                getter = {int: 'getint',
+                          float: 'getfloat',
+                          bool: 'getboolean',
                          }[opt_type]
             except KeyError:
-                getter = self.config.get
-            return getter(self.section, setting)
-        else:
+                getter = 'get'
+            v = self.root.call(Event(self.section, setting, default=fallback), getter, target='config')
+            val = v.value
+            #val = v
+            logging.debug("setting value from config component is %r" % val)
+
+        if val is None:
+            logging.debug("setting value is None. using defaults.")
             val = self.default_config.get(setting, fallback)
-            if opt_type in (None, bool) or isinstance(val, opt_type):
-                return val
-            try:
-                val = opt_type(val)
-            except ValueError:
-                return val
+            if not (opt_type in (None, bool) or isinstance(val, opt_type)):
+                try:
+                    val = opt_type(val)
+                except ValueError:
+                    pass
+
+        return val
 
     def __init__(self):
         super(collect_plugin, self).__init__()
@@ -89,14 +82,16 @@ class collect_plugin(Component):
             return True
         return False
 
-    def registered(self):
-        secs = self.get_setting('interval', opt_type=int)
-        self._timer = Timer(secs, GatherData, self.channel, persist=True).register(self)
+    def registered(self, component, manager):
+        if manager is not self and manager is self.manager and component is self:
+            secs = self.get_setting('interval', opt_type=int)
+            self._timer = Timer(secs, Event(), 'gather_data', t=self.name, persist=True).register(self)
 
-    def unregistered(self):
-        if self._timer:
-            self._timer.unregister()
-            self._timer = None
+    def unregistered(self, component, manager):
+        if manager is not self and manager is self.manager and component is self:
+            if self._timer:
+                self._timer.unregister()
+                self._timer = None
 
 
     def serialize(self, data):
@@ -118,8 +113,6 @@ class collect_plugin(Component):
 
         try:
             data = self.collect()
-            #data = self.serialize(self.collect())
-            #data = plugin()
         except Exception, e:
             logging.exception("error occurred while gathering data for sourcetype %s" % sourcetype)
             raise e
@@ -128,9 +121,11 @@ class collect_plugin(Component):
         #TODO: put stuff in spool
         #XXX: use spool as queue
 
-        spool[key] = (sourcetype, timestamp, data, extra)
+        #logging.debug("sourcetype: %r, timestamp: %r, extra: %r, data:\n%s" % (sourcetype, timestamp, pformat(extra), pformat(data)))
+        logging.debug("sourcetype: %r, timestamp: %r, extra: %r" % (sourcetype, timestamp, pformat(extra)))
+        #spool[key] = (sourcetype, timestamp, data, extra)
         
-        self.persist_queue.put(key)
+        #self.persist_queue.put(key)
 
     #def gather_data(self):
         #timestamp = time()
@@ -199,6 +194,8 @@ class plugin_manager(Component):
             return
 
         entry_points = self._entry_points
+        if entry_points is None:
+            return
         if not hasattr(entry_points, '__iter__'):
             entry_points = [entry_points]
             
@@ -222,7 +219,8 @@ class plugin_manager(Component):
             for py_file in glob(os.path.join(path, '*.py')):
                 try:
                     module_name = os.path.basename(py_file[:-3])
-                    # if it's already loaded, move on 
+                    if module_name.startswith('_'):
+                        continue
                     if module_name in sys.modules:
                         self._modules.add(sys.modules[module_name])
                         continue
@@ -234,68 +232,45 @@ class plugin_manager(Component):
                     logging.exception("Error loading module %s" % os.path.join(path, py_file))
 
 
-
-
     def load(self, base_class):
-        modules = self._modules
-        module = safeimport(name)
-        if module is not None:
+        def component_filter(member, module):
+            return ( isclass(member) \
+                    and issubclass(member, base_class) \
+                    and getmodule(member) is module \
+                    and not member.__name__.startswith('_')
+                   )
 
-            test = lambda x: isclass(x) \
-                    and issubclass(x, BaseComponent) \
-                    and getmodule(x) is module
-            components = [x[1] for x in getmembers(module, test)]
+        self.load_entry_points()
+        self.load_paths()
 
-            if components:
-                TheComponent = components[0]
+        plugins = self._plugins
 
-                component = TheComponent(
-                    *self._init_args,
-                    **self._init_kwargs
-                )
+        plugins.update((x[1](*self._init_args, **self._init_kwargs)
+                        for module in self._modules
+                        for x in getmembers(module,
+                                          partial(component_filter, module=module))))
 
-                if self._auto_register:
-                    component.register(self)
+        logging.info("in plugin_manager load. modules contains: %r" % self._modules)
+        logging.info("in plugin_manager load. plugins contains: %r" % plugins)
 
-                return component
+        for plugin in plugins:
+            #logging.debug("number of events in self: %r, in manager: %r, in root: %r" % (len(self), len(self.manager), len(self.root)))
+            #e = Event(plugin.section, 'enabled')
+            #val = self.fire(e, 'getboolean', target='config')
+            #self.manager.wait(e)
+            val = self.manager.call(Event(plugin.section, 'enabled'), 'getboolean', target='config')
+            #logging.debug("number of events in self: %r, in manager: %r, in root: %r" % (len(self), len(self.manager), len(self.root)))
+            #while self: self.flush()
+            #while self.manager: self.manager.flush()
+            #logging.debug("number of events in self: %r, in manager: %r, in root: %r" % (len(self), len(self.manager), len(self.root)))
+            #logging.info("iterating through plugins during load - val: %r, val.value: %r, e: %r" % (val, val.value, e))
+            if val.value:
+            #if self.config.getboolean(plugin.section, 'enabled'):
+                if plugin not in self:
+                    plugin.register(self)
 
+            elif plugin in self:
+                plugin.unregister()
 
-def find_plugins_in_path(search_path):
-    """
-    Discover plugins in any .py files in the given on-disk locations eg:
-    
-    find_plugins_in_path("/path/to/mymodule/plugins")
-    find_plugins_in_path(["/path/to/mymodule/plugins", "/some/more/plugins"])
-    
-    """
-    if isinstance(search_path, basestring):
-        search_path = [search_path]
-
-    for path in search_path:
-        log.debug("searching for plugins in %s" % search_path)
-        for py_file in glob(os.path.join(path, '*.py')):
-            try:
-                module_name = os.path.basename(py_file[:-3])
-                # if it's already loaded, move on 
-                if module_name in sys.modules:
-                    continue
-                
-                log.debug("Loading module %s" % py_file)
-                module = imp.load_source(module_name, py_file)
-            except:
-                log.error("Error loading module %s: %s" % (os.path.join(path, py_file), traceback.format_exc()))
-
-
-
-    def find_plugins_by_entry_point(entry_point_id, ws=master_working_set):
-        for entry in ws.iter_entry_points(entry_point_id):
-            log.debug('Loading plugin %s from %s', entry.name, entry.dist.location)
-
-            try:
-                entry.load(require=True)
-            except:
-                import traceback
-                log.error("Error loading plugin %s from %s: %s" % (entry.name, entry.dist.location, traceback.format_exc()))
-        
 
 

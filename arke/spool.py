32315c29a2f11a771e60b8eeb0e8b609ee0468d3
Dominic LoBue <dominic@geodelic.com>
2011-08-09 14:51:41 -0700

    update spooler to work without circuits

--- arke/spool.py
+++ arke/spool.py
@@ -7,28 +7,22 @@ logger = logging.getLogger(__name__)
 
 import bsddb3 as bsddb
 db = bsddb.db
-from circuits import Component, Event, handler
 
-class Spooler(Component):
-    channel = 'spooler'
-    def __init__(self, spool_file=None):
-        super(Spooler, self).__init__()
-
+class Spooler(object):
+    def __init__(self, config):
+        self.config = config
         self._txn_registry = {}
         self._dbenv = None
         self._db = None
-        self._spool_file = spool_file
 
 
-    @handler('started')
-    def open(self, *args, **kwargs):
+    def open(self, spool_file=None):
         if self._db is not None or self._dbenv is not None:
             logger.warn("Told to open an already-open spool again!")
             return
 
-        spool_file = self._spool_file
         if spool_file is None:
-            spool_file = self.root.call(Event('core', 'spool_file'), 'get', target='config').value
+            spool_file = self.config.get('core', 'spool_file', None)
             assert spool_file is not None, "no spool dir was given!"
 
         if '~' in spool_file:
@@ -48,74 +42,31 @@ class Spooler(Component):
         d = db.DB(e)
         d.set_q_extentsize(4096)
         d.set_pagesize(4096)
-        #d.set_re_len(4064)
 
         d.open(spool_file, db.DB_RECNO, db.DB_CREATE | db.DB_AUTO_COMMIT, 0600)
 
         self._dbenv = e
         self._db = d
+        return self
 
-        for key in d.keys():
-            self.manager.queue.put(key)
-        self.fire(Event(), 'persist', target='persist')
+    def keys(self):
+        return self._db.keys()
 
-
-    @handler('stopped')
-    def close(self, *args, **kwargs):
-        if self._txn_registry:
-            logger.error("Told to close when there are still open transactions!")
-            #XXX now what?
-            raise SystemExit
-
+    def close(self):
         self._db.close()
         self._dbenv.close()
         self._db = None
         self._dbenv = None
 
-
-
-    @handler('persist_data', target='persist', override=True)
     def append(self, sourcetype, timestamp, data, extra=None):
         s = dumps((sourcetype, timestamp, data, extra))
         rid = self._db.append(s)
-
-        self.fire(Event(rid=(rid, (sourcetype, timestamp, data, extra))), 'persist', target='persist')
         return rid
 
-    @handler('remove')
     def delete(self, rid):
         return self._db.delete(rid)
 
     def get(self, rid):
         data = self._db.get(rid)
         return loads(data)
-
-
-
-    #@handler('persist_data', target='persist', override=True)
-    #def append(self, sourcetype, timestamp, data, extra=None):
-        #txn = self._dbenv.txn_begin()
-        #s = self.serialize((sourcetype, timestamp, data, extra))
-        #rid = self._db.append(s, txn=txn)
-        #txn.commit()
-
-        #self.fire(Event(rid=(rid, (sourcetype, timestamp, data, extra))), 'persist', target='persist')
-        #return rid
-
-    def pop(self, rid):
-        txn = self._dbenv.txn_begin()
-        data = self._db.get(rid, txn=txn)
-        self._db.delete(rid, txn=txn)
-        self._txn_registry[rid] = txn
-        return loads(data)
-
-    def commit(self, rid):
-        if rid not in self._txn_registry:
-            logger.error("Was asked to commit a transaction for record id I have no record of: %r" % rid)
-            return None
-        txn = self._txn_registry.pop(rid)
-        txn.commit()
-
-
-
 

7d4f3727d6253c701272d668d680976d7746ee6d
Dominic LoBue <dominic@geodelic.com>
2011-08-09 14:55:13 -0700

    cruft removal

--- arke/plugins/persist/__init__.py
+++ arke/plugins/persist/__init__.py
@@ -1,5 +1,4 @@
 
 from .http import http_backend
 from .https import https_backend
-from .mongo import mongo_backend
 
--- arke/plugins/persist/base.py
+++ arke/plugins/persist/base.py
@@ -2,23 +2,11 @@
 import json
 
 from bson import json_util, BSON
-from circuits import Component, Event, Timer
 
 class ipersist(object):
     def __init__(self, config_parser):
         self.config = config_parser
         self.section = 'backend:%s' % self.__class__.__name__.replace('_backend', '')
-
-    def serialize(self, data):
-        if not self.format:
-            return data
-
-        if self.format.lower() == "json":
-            return json.dumps(data)
-        elif self.format.lower() == "bson":
-            return BSON.encode(data)
-        elif self.format.lower() == "extjson":
-            return json.dumps(data, default=json_util.default)
 
     def write(self, sourcetype, timestamp, data, hostname, extra):
         """

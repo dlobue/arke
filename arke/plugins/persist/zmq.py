1a7aa3538585498ef687824878ed3362214cb379
Dominic LoBue <dominic.lobue@gmail.com>
2012-09-02 06:46:41 -0700

    numerous fixes
    -fix deadlock that occurs if server dies while waiting for response
    -add in router socket to route checks to a specific server
    -explicitly close sockets and set LINGER to 0 to prevent socket leak
    -use multipart messages instead of json to transmit structure with clio
    -use predefined strings to signal success/failure of check call
    -... more i can't remember

--- arke/plugins/persist/zmq.py
+++ arke/plugins/persist/zmq.py
@@ -1,6 +1,7 @@
 
 from __future__ import absolute_import
 
+from os.path import getsize
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,17 +11,21 @@ try:
 except ImportError:
     from gevent_zeromq import zmq
 
-from gevent import sleep
+from gevent import sleep, getcurrent, Timeout
+from gevent.coros import BoundedSemaphore
 
 from arke.errors import PersistError
 from .base import ipersist
 
+ERROR = '\xff'
+SUCCESS = '\x00'
+NOTDONE = '\x01'
 
 class zmq_backend(ipersist):
     def __init__(self, *args, **kwargs):
         super(zmq_backend, self).__init__(*args, **kwargs)
         self.host = self.config.get(self.section, 'host')
-        self.port = None
+        self.port = 64000
 
         if self.config.has_option(self.section, 'port'):
             self.port = self.config.getint(self.section, 'port')
@@ -34,28 +39,65 @@ class zmq_backend(ipersist):
         #self.socket.connect('tcp://%s:%s' % (self.host, self.port))
 
     def get_connection(self):
-        #return self.socket
-        #TODO: socket pool
-        socket = self.context.socket(zmq.REQ)
-        socket.connect('tcp://%s:%s' % (self.host, self.port))
-        return socket
+        #TODO: figure out how zmq handles DNS records with multiple IPs
+        context = self.context.instance()
+        req = context.socket(zmq.REQ)
+        router = context.socket(zmq.ROUTER)
+        req.setsockopt(zmq.LINGER, 0)
+        router.setsockopt(zmq.LINGER, 0)
+        req.connect('tcp://%s:%s' % (self.host, self.port))
+        router.connect('tcp://%s:%s' % (self.host, self.port))
+        return req,router
 
 
     def batch_write(self, spool_file):
         spool_file.seek(0)
-        conn = self.get_connection()
-        conn.send_json(dict(action='persist',
-                            data=spool_file.read()))
+        req,router = self.get_connection()
+        buf = bytearray(getsize(spool_file.name))
+        spool_file.readinto(buf)
+        req.send_multipart(('persist', zmq.Message(buf)), copy=False)
 
-        receipt = conn.recv()
+        try:
+            with Timeout(60, PersistError):
+                while 1:
+                    try:
+                        address, receipt = req.recv_multipart(zmq.NOBLOCK)
+                        break
+                    except zmq.ZMQError, e:
+                        if e.strerror != 'Resource temporarily unavailable':
+                            raise e
+                        sleep(.5)
+        except:
+            req.close()
+            router.close()
+            raise
+        req.close()
+
+        check_msg = (address, '', 'check', receipt)
 
         while 1:
-            conn.send_json(dict(action='check',
-                                data=receipt))
-            response = conn.recv_json()
-            if response['status']:
-                break
-            elif response['status'] is None:
+            router.send_multipart(check_msg)
+
+            response = None
+            with Timeout(5, False):
+                while 1:
+                    try:
+                        _,_,response = router.recv_multipart(zmq.NOBLOCK)
+                        break
+                    except zmq.ZMQError, e:
+                        if e.strerror != 'Resource temporarily unavailable':
+                            raise e
+                        sleep(.5)
+
+            if response is None:
+                logger.warn("Didn't get any response to check request. will try again.")
+            elif response == SUCCESS:
+                router.close()
+                return
+            elif response == ERROR:
+                router.close()
                 raise PersistError
+            elif response == NOTDONE:
+                logger.debug('Server %s says %s is not done yet' % (address, receipt))
             sleep(5)
 
